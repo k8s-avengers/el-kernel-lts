64e64288b438090f35b28ec698db861feefd6761
Ricardo Pardini <ricardo@pardini.net>
2025-09-05 14:09:52 +0200

    1020: build nvidia open (580)/nonfree (535) versions matching nvidia's offered EL9 userspace
    - latest 535 for nonfree, except on aarch64 + 6.12+, then use latest 570
    - latest 580 for open, always

--- kernel.sh
+++ kernel.sh
@@ -19,12 +19,6 @@ declare PX_FUSE_BRANCH="v3.1.0-rpm-fixes-btf-nodeps"
 # Different make rpm-pkg / make binrpm-pkg - 6.12+ can't build without a git tree; 6.1 doesn't build devel without binrpm-pkg
 declare MAKE_COMMAND_RPM="rpm-pkg"
 
-# Different NVIDIA settings for different kernel versions and arch
-declare NVIDIA_OPEN_BRANCH="570"                           # Open driver "Production Branch", see https://www.nvidia.com/en-us/drivers/unix/
-declare NVIDIA_NONFREE_RUN_URL="undefined-nonfree-run-url" # varies per-arch
-declare NVIDIA_NONFREE_STAGE="nvidianonfreebuilder"        # defaults to building the nonfree modules TODO not used yet
-declare NVIDIA_NONFREE_VERSION="undefined"
-
 # Determine the architecture we're running on, as that will be the target architecture for the kernel build.
 declare OS_ARCH="undefined" TOOLCHAIN_ARCH="undefined"
 OS_ARCH="$(uname -m)"
@@ -51,36 +45,6 @@ case "${KERNEL_MINOR}" in
 		PAHOLE_VERSION="v1.30"
 		PX_FUSE_BRANCH="v-aaaae3e-6.12-rpm-btf-fixes-2"
 		MAKE_COMMAND_RPM="binrpm-pkg"
-		;;
-esac
-
-# Decide nvidia nonfree stuff. This ends up all hardcoded, and _will_ break over time
-# Go find the latests builds via https://www.nvidia.com/en-us/drivers/ and https://us.download.nvidia.com/XFree86/Linux-x86_64/
-# or https://us.download.nvidia.com/XFree86/aarch64/ for arm64
-case "${TOOLCHAIN_ARCH}:${KERNEL_MINOR}:" in
-	"x86_64:1:") # 535, it probably builds for .1, as it works for aarch64. CONFIRMED
-		NVIDIA_NONFREE_VERSION="535.261.03"
-		NVIDIA_NONFREE_RUN_URL="https://us.download.nvidia.com/XFree86/Linux-x86_64/535.261.03/NVIDIA-Linux-x86_64-535.261.03.run"
-		# If not, bump to 570: https://us.download.nvidia.com/XFree86/Linux-x86_64/570.172.08/NVIDIA-Linux-x86_64-570.172.08.run
-		;;
-	"x86_64:12:") # 535 builds fine for .12 on amd64, but NOT on aarch64 - CONFIRMED
-		NVIDIA_NONFREE_VERSION="535.261.03"
-		NVIDIA_NONFREE_RUN_URL="https://us.download.nvidia.com/XFree86/Linux-x86_64/535.261.03/NVIDIA-Linux-x86_64-535.261.03.run"
-		# If not, bump to 570: https://us.download.nvidia.com/XFree86/Linux-x86_64/570.172.08/NVIDIA-Linux-x86_64-570.172.08.run
-		;;
-
-	"aarch64:1:") # 535 builds with .1 on arm64, but NOT with .12 - CONFIRMED
-		NVIDIA_NONFREE_VERSION="535.261.03"
-		NVIDIA_NONFREE_RUN_URL="https://us.download.nvidia.com/XFree86/aarch64/535.261.03/NVIDIA-Linux-aarch64-535.261.03.run"
-		;;
-	"aarch64:12:") # 570 builds fine for .12 on arm64 - CONFIRMED
-		NVIDIA_NONFREE_VERSION="570.172.08"
-		NVIDIA_NONFREE_RUN_URL="https://us.download.nvidia.com/XFree86/aarch64/570.172.08/NVIDIA-Linux-aarch64-570.172.08.run"
-		;;
-
-	*)
-		echo "ERROR: Unsupported TOOLCHAIN_ARCH:${TOOLCHAIN_ARCH} and KERNEL_MINOR:${KERNEL_MINOR} combination for NVIDIA nonfree run URL." >&2
-		exit 1
 		;;
 esac
 
@@ -126,6 +90,111 @@ declare KERNEL_POINT_RELEASE="${KERNEL_POINT_RELEASE:-"${POINT_RELEASE}"}"
 declare MATRIX_ID_POINT_RELEASE="${MATRIX_ID//.y/.${KERNEL_POINT_RELEASE}}"
 echo "MATRIX_ID_POINT_RELEASE: ${MATRIX_ID_POINT_RELEASE}" >&2
 
+#### NVIDIA stuff.
+## The point here is to produce two RPMs:
+## nvidia-open (built from fully open source nvidia drivers; for modern Ampere+ GPUs; version 570+ should be fine, even 580)
+## nvidia-nonfree (built from the nvidia nonfree run file; for old GPUs; must be version 535)
+## On EL, the kernel driver has to match the userspace or it won't work.
+## So at kernel driver build time (now), we'll take a peek at what userspace (RHEL9) nvidia ships at that moment.
+declare NVIDIA_WANTED_EL_MAJOR_VERSION="9"
+## That is done by parsing https://developer.download.nvidia.com/compute/cuda/repos/rhel9/x86_64/ (or aarch64) and is fragile.
+## There might still be a mismatch, as nvidia might update the userspace after these kernel modules are built.
+
+declare nvidia_userspace_wanted_version="undefined" # global scope
+function find_nvidia_userspace_wanted_version() {
+	nvidia_userspace_wanted_version="undefined"
+	declare nvidia_el_version="${1}" # eg, "9"
+	shift
+	declare nvidia_arch="${1}" # eg, "x86_64" or "aarch64"
+	shift
+	declare nvidia_major_version="${1}" # eg, "535" or "570" or "580"
+	shift
+
+	echo "--> Finding nvidia userspace wanted version for EL${nvidia_el_version} ${nvidia_arch} ${nvidia_major_version}" >&2
+	declare nvidia_userspace_cache_file="nvidia-userspace-${nvidia_el_version}-${nvidia_arch}.html"
+	if [[ ! -f "${nvidia_userspace_cache_file}" ]]; then
+		echo "Getting nvidia userspace html from developer.download.nvidia.com" >&2
+		declare nvidia_userspace_url="undetermined"
+		case "${nvidia_arch}" in
+			"x86_64")
+				nvidia_userspace_url="https://developer.download.nvidia.com/compute/cuda/repos/rhel${nvidia_el_version}/x86_64/"
+				;;
+			"aarch64")
+				nvidia_userspace_url="https://developer.download.nvidia.com/compute/cuda/repos/rhel${nvidia_el_version}/sbsa/" # Why not aarch64, nvidia?
+				;;
+			*)
+				echo "ERROR: Unsupported nvidia_arch '${nvidia_arch}'" >&2
+				exit 1
+				;;
+		esac
+		curl -o "${nvidia_userspace_cache_file}.tmp" "${nvidia_userspace_url}"
+		mv -v "${nvidia_userspace_cache_file}.tmp" "${nvidia_userspace_cache_file}"
+	else
+		echo "Using disk cached ${nvidia_userspace_cache_file}" >&2
+	fi
+
+	# Parse the html file; produce nvidia_userspace_wanted_version
+	nvidia_userspace_wanted_version=$(cat "${nvidia_userspace_cache_file}" | grep "nvidia-driver-cuda-" | grep href | grep "${nvidia_arch}" | cut -d "'" -f 4 | grep "nvidia-driver-cuda-${nvidia_major_version}" | sort -V | tail -1 | cut -d "-" -f 4)
+
+	echo "--> Parsed nvidia userspace wanted version: '${nvidia_userspace_wanted_version}'" >&2
+
+	# Ensure it is sane
+	# It should have 2 dots in it
+	if [[ -z "${nvidia_userspace_wanted_version}" || "$(echo "${nvidia_userspace_wanted_version}" | grep -o '\.' | wc -l)" -ne 2 ]]; then
+		echo "ERROR: Could not find a sane nvidia userspace wanted version in ${nvidia_userspace_cache_file} for EL${nvidia_el_version} ${nvidia_arch} ${nvidia_major_version} - no two dots" >&2
+		exit 1
+	fi
+
+	# The first component (dot-separated) should be the same as nvidia_major_version
+	if [[ "$(echo "${nvidia_userspace_wanted_version}" | cut -d '.' -f 1)" != "${nvidia_major_version}" ]]; then
+		echo "ERROR: Could not find a matching nvidia userspace wanted version in ${nvidia_userspace_cache_file} for EL${nvidia_el_version} ${nvidia_arch} ${nvidia_major_version}, got '${nvidia_userspace_wanted_version}'" >&2
+		exit 1
+	fi
+
+	echo "--> Found nvidia userspace wanted version: '${nvidia_userspace_wanted_version}' for EL${nvidia_el_version} ${nvidia_arch} ${nvidia_major_version}" >&2
+}
+
+##
+## nvidia open, go for the latest 580 for all versions and arch'es
+##
+declare NVIDIA_OPEN_BRANCH="undetermined"
+find_nvidia_userspace_wanted_version "${NVIDIA_WANTED_EL_MAJOR_VERSION}" "${TOOLCHAIN_ARCH}" 580
+NVIDIA_OPEN_BRANCH="${nvidia_userspace_wanted_version}" # Not really a branch, actually a tag, works just as well
+echo "--> Using NVIDIA_OPEN_BRANCH: '${NVIDIA_OPEN_BRANCH}'" >&2
+
+##
+## nvidia nonfree, use 535, except on aarch64 with 12.y kernel, where we must use 570, as 535 doesn't build in that scenario.
+## It really doesn't make sense to use nonfree 570 on arm64 (just use open instead), but I gotta build "something" otherwise the build breaks.
+declare NVIDIA_NONFREE_MAJOR="535"
+if [[ "${TOOLCHAIN_ARCH}" == "aarch64" && "${KERNEL_MINOR}" -ge 12 ]]; then
+	NVIDIA_NONFREE_MAJOR="570"
+fi
+
+declare NVIDIA_NONFREE_VERSION="undefined"
+find_nvidia_userspace_wanted_version "${NVIDIA_WANTED_EL_MAJOR_VERSION}" "${TOOLCHAIN_ARCH}" "${NVIDIA_NONFREE_MAJOR}"
+NVIDIA_NONFREE_VERSION="${nvidia_userspace_wanted_version}"
+echo "--> Using NVIDIA_NONFREE_VERSION: '${NVIDIA_NONFREE_VERSION}'" >&2
+
+# Determine the download URL for the nonfree .run file, which varies per-arch.
+declare NVIDIA_NONFREE_RUN_URL="undefined-nonfree-run-url" # varies per-arch
+
+case "${TOOLCHAIN_ARCH}" in
+	"x86_64")
+		NVIDIA_NONFREE_RUN_URL="https://us.download.nvidia.com/XFree86/Linux-x86_64/${NVIDIA_NONFREE_VERSION}/NVIDIA-Linux-x86_64-${NVIDIA_NONFREE_VERSION}.run"
+		;;
+
+	"aarch64")
+		NVIDIA_NONFREE_RUN_URL="https://us.download.nvidia.com/XFree86/aarch64/${NVIDIA_NONFREE_VERSION}/NVIDIA-Linux-aarch64-${NVIDIA_NONFREE_VERSION}.run"
+		;;
+
+	*)
+		echo "ERROR: Unsupported TOOLCHAIN_ARCH:${TOOLCHAIN_ARCH} for NVIDIA nonfree run URL." >&2
+		exit 1
+		;;
+esac
+
+echo "--> Using NVIDIA_NONFREE_RUN_URL: '${NVIDIA_NONFREE_RUN_URL}'" >&2
+
 declare -a build_args=(
 	"--build-arg" "KERNEL_MAJOR=${KERNEL_MAJOR}"
 	"--build-arg" "KERNEL_MINOR=${KERNEL_MINOR}"
@@ -140,7 +209,6 @@ declare -a build_args=(
 	"--build-arg" "GCC_TOOLSET_NAME=${GCC_TOOLSET_NAME}"
 	"--build-arg" "PX_FUSE_BRANCH=${PX_FUSE_BRANCH}"
 	"--build-arg" "NVIDIA_NONFREE_RUN_URL=${NVIDIA_NONFREE_RUN_URL}"
-	"--build-arg" "NVIDIA_NONFREE_STAGE=${NVIDIA_NONFREE_STAGE}"
 	"--build-arg" "NVIDIA_NONFREE_VERSION=${NVIDIA_NONFREE_VERSION}"
 	"--build-arg" "NVIDIA_OPEN_BRANCH=${NVIDIA_OPEN_BRANCH}"
 )
